51baf0428634a2a922251419e90067184de00116
BT_3W2_30 <bryantv2789@nycstudents.net>
2023-09-26 12:00:25 -0400

    added a money back calculator

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,18 @@
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Goodbye world");
+import java.util.Scanner;
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("How much money do you owe?");
+        double moneyowed = scan.nextDouble();
+        System.out.println("How much money are you giving?");
+        double change= scan.nextDouble();
+        double moneyback = change - moneyowed;
+        System.out.println("Here is your change: $" + moneyback);
+
+
     }
 }
